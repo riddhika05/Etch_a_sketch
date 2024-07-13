443a983db83a0db52e7fb332311d74becb86c82f
Riddhika Arora <173531487+riddhika05@users.noreply.github.com>
2024-07-14 01:17:09 +0530

    styled the pop up

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 let sq=16;
+function p(){
 let no=prompt("squares","16")
  sq=parseInt(no);
+}
 const container=document.querySelector(".container");
-
+const sqSel=document.querySelector(".squares");
+sqSel.addEventListener('click',p)
 for(let i=0;i<sq*sq;i++)
 {
     const div=document.createElement("div");
@@ -13,9 +16,7 @@ for(let i=0;i<sq*sq;i++)
     div.style.opacity='0.4';
     container.appendChild(div);    
     div.addEventListener('mouseover',()=>{ div.style.backgroundColor="grey"});
-       
-   
-   
-
 }
+
+
 
