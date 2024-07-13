4d1833a8e4f541ecc873c29e5ffd1ccc71485d1e
Riddhika Arora <173531487+riddhika05@users.noreply.github.com>
2024-07-13 22:26:03 +0530

    REpositioned heading and set container width to 960px

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ for(let i=0;i<16;i++)
 {
     const div=document.createElement("div");
     div.style.backgroundColor = 'white';
-    div.style.width = '100px';
-    div.style.height = '100px';
+    div.style.width = '200px';
+    div.style.height = '200px';
     div.style.opacity='0.4';
     container.appendChild(div);
 }
