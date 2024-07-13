96b48e3d6844e6c26667bd2c176b122ffb3ddfde
Riddhika Arora <173531487+riddhika05@users.noreply.github.com>
2024-07-13 23:10:54 +0530

    Making a grid of user given number of squares in the same fixed container space

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
+let no=prompt("squares","0")
+let sq=parseInt(no);
 const container=document.querySelector(".container");
-for(let i=0;i<16;i++)
+for(let i=0;i<sq*sq;i++)
 {
     const div=document.createElement("div");
+    let dim=960/sq;
     div.style.backgroundColor = 'white';
-    div.style.width = '200px';
-    div.style.height = '200px';
+    div.style.width = dim+'px';
+    div.style.height =dim+'px';
     div.style.opacity='0.4';
     container.appendChild(div);
 }
