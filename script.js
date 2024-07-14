17188e7318447c9fca224e84dfbf2681ada8fd52
Riddhika Arora <173531487+riddhika05@users.noreply.github.com>
2024-07-14 23:01:55 +0530

    Removed overlay as it served no purpose

--- script.js
+++ script.js
@@ -3,15 +3,13 @@ let inp;
 const container = document.querySelector(".container");
 const sqSel = document.querySelector(".squares");
 const popup = document.querySelector(".popup");
-const overlay = document.querySelector("#overlay");
 const input = document.querySelector("#sqinput");
 const color=document.querySelector(".color");
 function p() {
     popup.classList.add('active');
     color.style.display="none";
-    // overlay.classList.add('active');
     input.focus();
-    // input.value='';
+   
 }
 input.addEventListener('keypress',function(event)
 {
@@ -35,23 +33,14 @@ for (let i = 0; i < sq * sq; i++) {
     container.appendChild(div);
     popup.style.display="none"
     color.style.display="flex";
-    div.addEventListener('mouseover', () => { div.style.backgroundColor = "grey" });
-    
+    div.addEventListener('mouseover', () => { div.style.backgroundColor = "grey" });   
+    }
+    colorPick();
 }
+function colorPick()
+{
+   color.addEventListener('click',()=>{console.log("q")}) 
 }
-// function initial()
-// {
-//     for (let i = 0; i < 16*16; i++) {
-//         const div = document.createElement("div");
-//         let dim = 60;
-//         div.style.backgroundColor = 'white';
-//         div.style.width = dim + 'px';
-//         div.style.height = dim + 'px';
-//         div.style.opacity = '0.4';
-//         container.appendChild(div);
-//         div.addEventListener('mouseover', () => { div.style.backgroundColor = "grey" });
-//     }
-// }
 
 
 
