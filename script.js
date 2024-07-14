30f488f685f5afb31f3fe9f8b29d16f5bf3a6b59
Riddhika Arora <173531487+riddhika05@users.noreply.github.com>
2024-07-14 20:29:59 +0530

    Button noe triggers popup which makes grid asper user input

--- script.js
+++ script.js
@@ -1,21 +1,41 @@
-let sq=16;
-function p(){
-let no=prompt("squares","16")
- sq=parseInt(no);
+let sq ;
+let inp;
+sq=16;
+sqGen();
+const popup = document.querySelector(".popup");
+const overlay = document.querySelector("#overlay");
+const input = document.querySelector("#sqinput");
+
+function p() {
+    popup.classList.add('active');
+    // overlay.classList.add('active');
+    input.focus();
+    // input.value='';
 }
-const container=document.querySelector(".container");
-const sqSel=document.querySelector(".squares");
-sqSel.addEventListener('click',p)
-for(let i=0;i<sq*sq;i++)
+input.addEventListener('keypress',function(event)
 {
-    const div=document.createElement("div");
-    let dim=960/sq;
+    if (event.key === 'Enter')
+        inp=input.value; 
+        console.log(inp);
+        sqGen();
+})
+
+const container = document.querySelector(".container");
+const sqSel = document.querySelector(".squares");
+sqSel.addEventListener('click', p)
+function sqGen(){
+sq=inp;
+console.log(sq);
+for (let i = 0; i < sq * sq; i++) {
+    const div = document.createElement("div");
+    let dim = 960 / sq;
     div.style.backgroundColor = 'white';
-    div.style.width = dim+'px';
-    div.style.height =dim+'px';
-    div.style.opacity='0.4';
-    container.appendChild(div);    
-    div.addEventListener('mouseover',()=>{ div.style.backgroundColor="grey"});
+    div.style.width = dim + 'px';
+    div.style.height = dim + 'px';
+    div.style.opacity = '0.4';
+    container.appendChild(div);
+    div.addEventListener('mouseover', () => { div.style.backgroundColor = "grey" });
+}
 }
 
 
